bdda891d086ad42672cb5feb8ef8385e208e2d46
750947Student <750947@student.inholland.nl>
2026-05-07 10:48:19 +0200

    HI TEST

--- Chapeau/Controllers/HomeController.cs
+++ Chapeau/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Chapeau.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult PrivacY()
         {
             return View();
         }
