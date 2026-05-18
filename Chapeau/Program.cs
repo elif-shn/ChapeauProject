aff088c5fe817d732c0a8972b9341dded7349ade
Mehedi160402 <740986@student.inholland.nl>
2026-05-18 13:19:14 +0200

    I did management

--- Chapeau/Program.cs
+++ Chapeau/Program.cs
@@ -9,7 +9,7 @@ namespace Chapeau
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+           
             builder.Services.AddControllersWithViews();
             builder.Services.AddScoped<IMenuItemRepository, DbMenuItemRepository>();
             builder.Services.AddScoped<IOrderRepository, DbOrderRepository>();
@@ -21,11 +21,11 @@ namespace Chapeau
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            
                 app.UseHsts();
             }
 
