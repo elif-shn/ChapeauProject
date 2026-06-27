339fe074e2725956c3f3013ad6637c226fda75a4
Nahidul7710 <742950@student.inholland.nl>
2026-06-28 01:35:10 +0200

    finalupdate

--- Chapeau/Services/Interfaces/IPaymentService.cs
+++ Chapeau/Services/Interfaces/IPaymentService.cs
@@ -5,7 +5,15 @@ namespace Chapeau.Services.Interfaces
     public interface IPaymentService
     {
         PaymentViewModel GetDashboard();
+
         PaymentViewModel GetBillByTableId(int tableId);
+
         void ConfirmPayment(PaymentViewModel viewModel);
+
+        SplitPaymentViewModel GetSplitPaymentByTableId(int tableId);
+
+        void ConfirmSplitEqualPayment(SplitPaymentViewModel viewModel);
+
+        void ConfirmSplitDifferentPayment(SplitPaymentViewModel viewModel);
     }
 }
