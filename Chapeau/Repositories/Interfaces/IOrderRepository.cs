bb745ed97e38af481e42c46ae7273870a4e9af18
Mehedi160402 <740986@student.inholland.nl>
2026-06-28 15:32:37 +0200

    done

--- Chapeau/Repositories/Interfaces/IOrderRepository.cs
+++ Chapeau/Repositories/Interfaces/IOrderRepository.cs
@@ -12,8 +12,8 @@ public interface IOrderRepository
     public void CreateOrderWithItems(Order order);
     public void AddItemsToExistingOrder(Order order, List<OrderItem> items);
     public Order GetActiveOrderForTable(int tableId);
-    List<Order> GetRunningOrders();
-
+   /* List<Order> GetRunningOrders();*/
+    
     List<Order> GetRunningTableOrders(int tableId);
     List<Order> GetActiveDrinkOrders(int tableId);
     List<Order> GetActiveFoodOrders(int tableId);
@@ -21,10 +21,10 @@ public interface IOrderRepository
     List<Order> GetFinishedOrders();
     void UpdateOrderStatus(Order order, OrderStatus status);
     void UpdateOrderItemStatus(OrderItem orderItem, OrderItemStatus status);
-    Order? GetOrderById(Order order);
+   /* Order? GetOrderById(Order order);
     public void CreateOrderWithItems(Order order);
     void AddItemsToExistingOrder(Order order, List<OrderItem> items);
-    Order GetActiveOrderForTable(int tableId);
+    Order GetActiveOrderForTable(int tableId);*/
     void MarkOrderAsServed(int orderId);
 }
 
